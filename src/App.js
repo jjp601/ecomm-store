3df39663c78df346b87e3bdc6a77c28df505bd77
Jon Powell <jpowell68601@gmail.com>
2021-05-09 13:40:41 -0500

    Setup Sign Up Page and reusable Form Input and button components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
+import SignInPage from './pages/SignInPage';
 import Header from './components/Header';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/SignInPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Signin from '../components/SignIn';
+
+import '../styles/SignInPage.scss';
+
+const SignInPage = () => {
+    return (
+        <div className='sign-in-and-sign-up'>
+            <Signin />
+        </div>
+    );
+};
+
+export default SignInPage;
